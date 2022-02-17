313c0c82e3356102046d236d3a3774c59b4f7090
Jayone1202 <dolt1202@gmail.com>
2022-02-17 21:29:18 +0900

    feat: add to createNewBlock

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
@@ -5,8 +7,18 @@ class Block {
   public data: string;
   public timestamp: number;
 
+  //static metshod
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
+    // hash는 모든 속성을 엄청 길고 수학적으로 이상한 하나의 문자열로 결합한 것
     hash: string,
     previousHash: string,
     data: string,
@@ -24,6 +36,43 @@ const genesisBlock: Block = new Block(0, "20202020202", "", "Hello", 123456);
 
 let blockchain: [Block] = [genesisBlock];
 
-console.log(blockchain);
+// 현재 블록체인 구조를 가져온다.
+const getBlockchain = (): Block[] => blockchain;
+
+// 가장 최근에 추가된 블록체인을 가져온다.
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+// 현재 시간을 밀리초로 계산
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+// 블록체인 생성 (파라미터는 문자열 타입) Block을 리턴한다.
+const createNewBlock = (data: string): Block => {
+  // 이전 블록체인은 마지막 블록체인이다.
+  const previousBlock: Block = getLatestBlock();
+  // 새로운 인덱스는 이전 블록체인 index에 + 1 해준다.
+  const newIndex: number = previousBlock.index + 1;
+  // 새로운 시간을 생성한다.
+  const newTimestamp: number = getNewTimeStamp();
+
+  // 새로운 hash를 생성한다.
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+
+  // 새로운 블록을 생선한다.
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 
 export {};
